0fed58f3d6c3d6411411cfbf4694a885769683e6
Laura Blázquez <laura.blazquez.7e7@itb.cat>
2024-01-08 13:35:55 +0100

    DLL upload

---
+++ UtilsLibrary/Utils.cs
@@ -0,0 +1,47 @@
+﻿namespace UtilsLibrary
+{
+    public class Utils
+    {
+        public static string ValidateOdd(int num)
+        {
+            const int Module = 2;
+            if (num % Module == 0) return "No és senar";
+            return "És senar";
+        }
+        public static int Pow(int num, int exponent)
+        {
+            int result = num;
+            if (exponent == 0) return 1;
+            for (int i = 1; i < exponent; i++) result *= num;
+            return result;
+        }
+        public static int Randomizer(int min, int max) 
+        {
+            var random = new Random();
+            return random.Next(min, max+1);
+        }
+        public static void CountLetters(string text,ref int vowels, ref int cons)
+        {
+            text.ToLower();
+            text.Replace('á', 'a');
+            text.Replace('à', 'a');
+            text.Replace('è', 'e');
+            text.Replace('é', 'e');
+            text.Replace('í', 'i');
+            text.Replace('ò', 'o');
+            text.Replace('ó', 'o');
+            text.Replace('ú', 'u');
+
+            foreach (char letter in text) 
+            {
+                if (letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u') vowels++;
+                else if (letter != ' ') cons++;
+            }
+        }
+        public static bool ValidateNatural(int num)
+        {
+            if (num < 0) return false;
+            return true;
+        }
+    }
+}
